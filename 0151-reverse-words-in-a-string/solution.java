eb75f7b84e1e10db7d8318f92728d91fbe79db5a
vikky49 <vikky49@gmail.com>
2017-01-16 21:24:28 +0000

    Sync LeetCode submission Runtime - 76 ms (5.91%), Memory - N/A (N/A)

--- 0151-reverse-words-in-a-string/solution.java
+++ 0151-reverse-words-in-a-string/solution.java
@@ -1,31 +1,10 @@
-class Solution {
+public class Solution {
     public String reverseWords(String s) {
-
-        if(s == null || s.length() == 0) {
-            return s;
-        }
-
-        Stack<String> stack = new Stack<>();
-
-        String[] words = s.trim().split("\\s+");
-
-        for(String word : words) {
-             if (word.length() > 0) {
-                stack.push(word);
-             }
-        }
-
-        StringBuilder builder = new StringBuilder();
-
-        while (!stack.isEmpty()) {
-            builder.append(stack.pop());
-            //Now add the space 
-            if (!stack.isEmpty()) {
-                builder.append(" ");
-            }
-        }
-    
-      return builder.toString();
-        
+        String[] parts = s.trim().split("\\s+");
+        String out = "";
+         for (int i = parts.length - 1; i > 0; i--) {
+          out += parts[i] + " ";
+         }
+        return out + parts[0]; 
     }
 }
