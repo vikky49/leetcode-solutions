7a62740bfd9ba8c419c2b2b537d45bcd8f1a2d9d
vikky49 <vikky49@gmail.com>
2022-07-10 20:58:29 +0000

    Sync LeetCode submission Runtime - 1 ms (94.15%), Memory - 43.8 MB (5.39%)

--- 0107-binary-tree-level-order-traversal-ii/solution.java
+++ 0107-binary-tree-level-order-traversal-ii/solution.java
@@ -15,39 +15,45 @@
  */
 class Solution {
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<>();
-
-        if(root == null) {
-            return result;
-        }
-
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            List<Integer> level = new ArrayList<>();
-
-           for(int i =0 ; i < size; i ++) {
-
-            TreeNode node = queue.poll();
-
-            level.add(node.val);
-
-            if(node.left != null) {
-                queue.offer(node.left);
-            }
-
-            if(node.right != null) {
-                queue.offer(node.right);
-            }
-
-          }
-          result.add(level);
-        }
-         
-        Collections.reverse(result);
-        return result;
         
+        List<List<Integer>> levels = new ArrayList<>();
+        
+        if(root == null) {
+            return levels;
+        }
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        
+        // add the root element in the queue
+        queue.add(root);
+        
+        while(!queue.isEmpty()) {
+            
+            List<Integer> level = new ArrayList<>();
+            
+            //get the current size of the queue
+            int size = queue.size();
+                    
+            for(int i = 0 ; i < size ; i++ ) {
+            //Remove the element in the queue
+            TreeNode current = queue.remove();
+            level.add(current.val);     
+            
+            if(current.left != null) {
+                queue.add(current.left);
+            }
+            
+            if(current.right != null) {
+                queue.add(current.right);
+            }                     
+                
+          }
+            
+          levels.add(0,level);   
+                        
+        }
+        
+        return levels;
     }
+    
 }
