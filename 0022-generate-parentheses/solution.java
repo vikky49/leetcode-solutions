cd88d66473879003680f904cd94c65b2f4684ddc
vikky49 <vikky49@gmail.com>
2024-04-27 18:20:32 +0000

    Sync LeetCode submission Runtime - 2 ms (45.36%), Memory - 43.6 MB (17.99%)

--- 0022-generate-parentheses/solution.java
+++ 0022-generate-parentheses/solution.java
@@ -1,49 +1,30 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        List<String> result = new ArrayList<>();
-        StringBuilder slate = new StringBuilder();
-
-        backtrack(result, slate, n, n);
-
+        var result = new ArrayList<String>();
+        helper("",n,n,result);
         return result;
     }
-
-    void backtrack(
-        List<String> result,
-        StringBuilder slate,
-        int open,
-        int close
-    ) {
-
-        //Base case 
-     
-         if(open == 0 && close == 0) {
-            result.add(slate.toString());
-            return;
-         }
-
-        //Recursive case 
-        //At every step there are 2 steps to make a choice 
-         
-         if(open > 0) {
-            //append the open brace when we still have open left 
-            slate.append('(');
-            backtrack(result, slate, open-1, close);
-            //backtrack - where we delete and bring back the slate to the last state
-
-            slate.deleteCharAt(slate.length()-1);
-         }
-
-         if(close > open) {
-            slate.append(')');
-            backtrack(result, slate, open, close-1);
-            //backtrack - where we delete and bring back the slate to the last state
-
-            slate.deleteCharAt(slate.length()-1);
-         }
-
-    }
-
     
+    private void helper(
+        String slate,
+        int leftPairs,
+        int rightPairs,
+        List<String> results
+    ) {
+        
+        //backtracking case 
+        if (leftPairs > rightPairs || leftPairs < 0 || rightPairs < 0) {
+            return;
+        }
+        
+        //Base case 
+        if (leftPairs == 0 && rightPairs == 0 ) {
+            results.add(slate);
+            return;
+        }
+        
+        helper(slate + "(" , leftPairs-1, rightPairs, results);
+        helper(slate + ")", leftPairs, rightPairs-1,results);
+        
+    }
 }
-
