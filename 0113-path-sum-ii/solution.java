098603935932704a12e8a7ba7194548810b14f87
vikky49 <vikky49@gmail.com>
2024-06-01 20:58:53 +0000

    Sync LeetCode submission Runtime - 1 ms (99.75%), Memory - 44.2 MB (95.07%)

--- 0113-path-sum-ii/solution.java
+++ 0113-path-sum-ii/solution.java
@@ -15,55 +15,50 @@
  */
 class Solution {
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+       List<List<Integer>> result = new ArrayList<>();
+       List<Integer> paths = new ArrayList<>();
+
+       // Edge case 
+       if(root == null) {
+         return result;
+       }  
+
+       dfs(root,targetSum,paths,result);
+
+       return result;
         
-        List<List<Integer>> results = new ArrayList<>();
-        List<Integer> slate = new ArrayList<>();
-        
-        // Edge Case 
-        if(root == null) {
-            return results;
-        }
-        
-        dfs(root,targetSum,slate,results);
-        
-        return results;
     }
-    
-    
-        private void dfs(
-            TreeNode node, 
-            int target, 
-            List<Integer> slate,
-            List<List<Integer>> results ) {
-            
-            //first add the value 
-            slate.add(node.val);
-            
-            target = target - node.val;
-            
-            //Base Case 
-        
-            // if node is a leaf node 
-            
-            if(node.left == null && node.right == null) {
-                
-                if(target == 0) {
-                    results.add(new ArrayList<>(slate));
-                }                
-            }
-            //Recursive case 
-            
-            if(node.left != null ) {
-                dfs(node.left,target, slate, results);
-            }
-            
-            if(node.right != null) {
-                dfs(node.right, target, slate, results);
-            }
-            
-            slate.remove(slate.size()-1);
-            
-        }
-        
-        
-  }
+
+    private void dfs(
+        TreeNode node,
+        int targetSum,
+        List<Integer> paths,
+        List<List<Integer>> result
+    ) {
+
+        //add the current node val to the paths
+        paths.add(node.val);
+        targetSum = targetSum - node.val;
+
+        // Base case (if the current node is the leaf)
+
+         if(node.left == null && node.right == null) {
+             if(targetSum == 0) {
+                result.add(new ArrayList(paths));
+             }
+         }
+
+        //Recursive case 
+
+         if(node.left != null) {
+            dfs(node.left,targetSum,paths,result);
+         }
+
+         if(node.right != null) {
+            dfs(node.right,targetSum,paths,result);
+         }
+
+        paths.remove(paths.size()-1); 
+
+    }
+}
