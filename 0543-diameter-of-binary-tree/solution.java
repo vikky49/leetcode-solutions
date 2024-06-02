19e11415923304a2aeea44a6fe4bf1b61ea4f5af
vikky49 <vikky49@gmail.com>
2024-06-02 18:08:25 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 44.8 MB (64.19%)

--- 0543-diameter-of-binary-tree/solution.java
+++ 0543-diameter-of-binary-tree/solution.java
@@ -14,46 +14,51 @@
  * }
  */
 class Solution {
+   
+   private int diameter;
+
     public int diameterOfBinaryTree(TreeNode root) {
-        
-        if (root == null) {
+    
+        //Edge case
+        if(root == null) {
             return 0;
         }
+        dfs(root);
+        return diameter;
         
-        int[] diameter = new int[1];
-        dfs(root,diameter);
-        return diameter[0];
-    
     }
-    
-    private int dfs(TreeNode node, int[] result) {
-        
-        //Bottom up recursion
-        //Base case 
-        if(node.left == null && node.right == null) {
-            return 0;
+
+    private int dfs(
+        TreeNode node) {
+
+            //Base case 
+            // if the node is the leaf
+            if(node.left == null && node.right == null) {
+                return 0;
+            }
+
+            //Recursive case 
+            
+            int height = 0;
+            int longestPath = 0;
+
+            if(node.left != null) {
+                int leftHeight = 1 + dfs(node.left);
+                height = Math.max(height,leftHeight);
+                longestPath = leftHeight;
+            }
+
+            if(node.right != null) {
+              int rightHeight = 1 + dfs(node.right);
+              height = Math.max(height,rightHeight);
+              longestPath += rightHeight;
+            }
+
+            if(longestPath > diameter) {
+                diameter = longestPath;
+            }
+
+            return height;
+
         }
-        
-        //Recursive case
-        
-        int height = 0;
-        int diameter = 0;
-        
-        if (node.left != null) {
-            int leftHeight = dfs(node.left,result);
-            diameter += 1 + leftHeight;
-            height = Math.max(height, 1+leftHeight);
-        }
-        
-        if (node.right != null) {
-            int rightHeight = dfs(node.right, result);
-            diameter += 1 + rightHeight;
-            height = Math.max(height, 1+rightHeight);
-        }
-        
-        result[0] = Math.max(result[0],diameter);
-        
-        return height;
-                
-    }
 }
