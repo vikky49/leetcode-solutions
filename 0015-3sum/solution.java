9e9e15460112b1be659e093380db75ad11817c73
vikky49 <vikky49@gmail.com>
2025-05-04 17:52:13 +0000

    Sync LeetCode submission Runtime - 24 ms (98.34%), Memory - 51.4 MB (69.94%)

---
+++ 0015-3sum/solution.java
@@ -0,0 +1,57 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length < 3) {
+            return res;
+        }
+        Arrays.sort(nums);
+        int n = nums.length;
+        for (int i = 0; i < n - 2; i++) {
+            //skip duplicates for first element
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            
+            //optimization
+            // if the first element is greater than zero then we might never find the solution
+            if(nums[i] > 0) {
+                break;
+            }
+            
+            int left = i+1;
+            int right = n-1;
+            
+            while (left < right ) {
+                int sum = nums[i] + nums[left] + nums[right];
+                
+                if(sum < 0) {
+                    left++;
+                } else if (sum > 0) {
+                    right--;
+                } else {
+                    //Found a triplet
+                    res.add(Arrays.asList(
+                            nums[i],
+                            nums[left],
+                            nums[right]
+                    ));
+                    
+                    //skip duplicates on both sides 
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    
+                    //Move both the pointers
+                    left++;
+                    right--;
+                }
+            }
+
+        }
+        return res;
+    }
+}
