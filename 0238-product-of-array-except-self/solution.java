99e657be383c8a84ceac1f5cf189232f048e2158
vikky49 <vikky49@gmail.com>
2025-07-15 14:57:13 +0000

    Sync LeetCode submission Runtime - 2 ms (87.03%), Memory - 55.5 MB (61.86%)

--- 0238-product-of-array-except-self/solution.java
+++ 0238-product-of-array-except-self/solution.java
@@ -1,22 +1,26 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        //store the result
         int[] result = new int[n];
 
-        //Lets calculate the left product first
-        result[0] = 1;
-        for(int i = 1; i < n ; i++) {
+        //Step1: Calculate the left side of the things 
+        result[0] = 1; 
+        int i = 1; 
+        // since we start with the left most we ignore the zeroth index as there is nothing before that number to Calculate
+        while(i < n) {
             result[i] = result[i-1] * nums[i-1];
+            i++; 
         }
 
-        //calculate the right product 
+        //Do the right product 
         int rightProduct = 1;
-        for (int i = n-1; i >=0 ; i --) {
+        i = n-1;
+        while (i >= 0) {
+            //First multiply the right numner 
             result[i] = result[i] * rightProduct;
             rightProduct = rightProduct * nums[i];
+            i--;
         }
-        
         return result;
     }
 }
