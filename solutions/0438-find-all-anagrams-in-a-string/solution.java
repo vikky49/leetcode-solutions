5e17e258ca2e3f4cdfcc1bfcfb0b756c5514bb39
vikky49 <vikky49@gmail.com>
2025-07-20 16:51:13 +0000

    [LeetCode Sync] Runtime - 81 ms (21.80%), Memory - 45.3 MB (35.53%)

--- solutions/0438-find-all-anagrams-in-a-string/solution.java
+++ solutions/0438-find-all-anagrams-in-a-string/solution.java
@@ -1,75 +1,52 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
 
-        //Edge base case
-        if(p.length() > s.length()) {
+        //Edge cases 
+        if(s == null || s.length() == 0 || 
+           p == null || p.length() == 0 || 
+           s.length() < p.length()) {
+           
             return List.of();
         }
 
-        //Sliding window technique
+        List<Integer> result = new ArrayList<>();
+        Map<Character,Integer> pfreqMap = new HashMap<>();
+        Map<Character,Integer> windowFreq = new HashMap<>();
 
-        Map<Character,Integer> p_map = new HashMap<>();
-        Map<Character,Integer> s_map = new HashMap<>();
-        List<Integer> output = new ArrayList<>();
-
-        int k = p.length();
-
-        //Step 1 : Initialize the pmap
-
-        for (int i = 0; i < p.length(); i++) {
-            Character p_char = p.charAt(i);
-            if (p_map.containsKey(p_char)) {
-                int frequency = p_map.get(p_char);
-                p_map.put(p_char, frequency + 1);
-            } else {
-                p_map.put(p_char, 1);
-            }
+        for(char c: p.toCharArray()) {
+            pfreqMap.put(c, pfreqMap.getOrDefault(c, 0)+1);
         }
 
-        //Initialize the first window
+        int left = 0; 
+        int windowSize = p.length();
 
-        for (int i = 0; i < k; i++) {
-            Character s_char = s.charAt(i);
-            if (s_map.containsKey(s_char)) {
-                int frequency = s_map.get(s_char);
-                s_map.put(s_char, frequency + 1);
-            } else {
-                s_map.put(s_char, 1);
+        for(int right = 0; right < s.length(); right++) {
+            char rightChar = s.charAt(right);
+            windowFreq.put(rightChar, windowFreq.getOrDefault(rightChar, 0) + 1);
+
+            //check the window size now 
+            //if the windowSize is greater === pop out the left char
+            if(right - left + 1 > windowSize) {
+                char leftChar = s.charAt(left);
+                windowFreq.put(leftChar, windowFreq.get(leftChar) - 1 );
+
+                //check the map and remove
+
+                if(windowFreq.get(leftChar) == 0 ) {
+                    windowFreq.remove(leftChar);
+ 
+                }
+                //adjust the window size
+                left ++;
             }
-        }
 
-        if(p_map.equals(s_map)) {
-            output.add(0);
-        }
-
-        //Slide the window to the right
-        //Remove the left most element and add the right most element
-
-        for (int i = k; i < s.length(); i++) {
-            Character leftElement = s.charAt(i - k);
-            int frequency = s_map.get(leftElement) - 1;
-            if (frequency == 0) {
-                s_map.remove(leftElement);
-            } else {
-                s_map.put(leftElement, frequency);
-            }
-            
-            //Now add the right element
-            Character nextElement = s.charAt(i);
-            if(s_map.containsKey(nextElement)){
-                int freq = s_map.get(nextElement);
-                s_map.put(nextElement, freq + 1);
-            } else {
-                s_map.put(nextElement, 1);
-            }
-            
-            //Compare the maps and get the index
-            if(s_map.equals(p_map)) {
-                output.add(i - k + 1);
+            if(right -left + 1 == windowSize && windowFreq.equals(pfreqMap)) {
+                result.add(left);
             }
 
         }
-        return output;
+
+        return result;
         
     }
 }
