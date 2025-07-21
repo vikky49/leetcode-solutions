963136328e48aa5e6c68ef132cef63dc3e82b33f
vikky49 <vikky49@gmail.com>
2025-07-21 18:20:38 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.6 MB (10.38%)

---
+++ 0074-search-a-2d-matrix/solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+        if(matrix == null || 
+           matrix.length == 0 || 
+           matrix[0].length == 0) {
+           
+            return false;
+        }
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int left = 0;
+        int right = rows * cols -1;
+
+        //Do a binary search
+        while(left <= right) {
+
+            int mid = left + (right-left)/2;
+            int midValue = matrix[mid/cols][mid%cols];
+            
+            if(midValue == target) {
+                return true;
+            } else if(midValue < target) {
+                left = mid + 1;
+            } else { 
+                right = mid -1;
+            }
+
+        }
+        return false;
+    }
+}
