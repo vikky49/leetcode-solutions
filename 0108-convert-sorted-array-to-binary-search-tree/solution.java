07bd928b61155a984fb7b33884af9ef83d82b43f
vikky49 <vikky49@gmail.com>
2025-08-22 13:54:31 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.2 MB (57.35%)

--- 0108-convert-sorted-array-to-binary-search-tree/solution.java
+++ 0108-convert-sorted-array-to-binary-search-tree/solution.java
@@ -14,20 +14,27 @@
  * }
  */
 class Solution {
-       public TreeNode sortedArrayToBST(int[] nums) {
-        return recursive_helper(nums, 0, nums.length-1);
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return buildBST(nums, 0, nums.length-1);
+        
     }
 
+    private TreeNode buildBST(
+        int[] nums,
+        int left,
+        int right) {
 
-    private TreeNode recursive_helper(int[] nums, int start, int end) {
-        if (start > end) {
+         if(left > right) {
             return null;
-        }
+         }  
 
-        int mid = start + (end - start) / 2;
-        TreeNode root = new TreeNode(nums[mid]);
-        root.left = recursive_helper(nums, start, mid - 1);
-        root.right = recursive_helper(nums, mid + 1, end);
-        return root;
+         int mid = left + (right - left) / 2 ;
+         TreeNode root = new TreeNode(nums[mid]);
+
+         root.left = buildBST(nums, left, mid-1);
+         root.right = buildBST(nums, mid+1, right);
+
+         return root; 
+
     }
 }
