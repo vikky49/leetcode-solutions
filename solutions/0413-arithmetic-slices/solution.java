6e7b89404ef75c88d0a8a07b8578d7953d3d4a98
vikky49 <vikky49@gmail.com>
2025-08-31 22:31:25 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.5 MB (46.89%)

---
+++ solutions/0413-arithmetic-slices/solution.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int numberOfArithmeticSlices(int[] nums) {
+        //Base case 
+        if(nums.length < 3) {
+            return 0;
+        }
+
+        int totalCount = 0;
+        int left = 0;
+
+        while(left <= nums.length - 3) {
+            int right = left + 1;
+            int diff =  nums[right] - nums[left];
+
+            //expand the window
+            while(right+1 < nums.length && 
+                  nums[right +1] - nums[right] == diff) {
+                   //Expand the window
+                   right ++;
+                  }
+
+            //calculate the number of subarrays in the window (left,right)      
+
+            int windowLength = right - left + 1;
+
+            if(windowLength >=3) {
+                int subarrays = (windowLength - 2) * (windowLength - 1) / 2;
+                totalCount += subarrays;
+            }
+
+            left = right;
+
+        }
+        return totalCount;
+        
+    }
+}
