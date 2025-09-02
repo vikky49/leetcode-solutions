9c866dfbeec8e6f301639cf4676a02aa640a3805
vikky49 <vikky49@gmail.com>
2025-09-02 18:25:21 +0000

    Sync LeetCode submission Runtime - 1 ms (92.82%), Memory - 44.5 MB (64.58%)

--- 0046-permutations/solution.java
+++ 0046-permutations/solution.java
@@ -1,41 +1,56 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-
-     //For all the exhaustive problems we need two things 
       
-    var result = new ArrayList<List<Integer>>();
+       List<List<Integer>> result = new ArrayList<>();
     
-    //A temporary snapshot of the data for the dfs
-
-    var slate = new ArrayList<Integer>();
-
-     backtrack(nums,0,slate,result);
-
-     return result;
+       List<Integer> slate = new ArrayList<>();
+       boolean[] used = new boolean[nums.length];
+       backtrack(
+           nums,
+           result,
+           slate,
+           used
+       );
+      
+       return result;
         
     }
-
+    
     private void backtrack(
-        int[] nums,
-        int pos,
-        List<Integer> slate,
-        List<List<Integer>> result
+      int[] nums,
+      List<List<Integer>> result,
+      List<Integer> slate,
+      boolean[] used
     ) {
-
-        //Base case 
-
-        if(nums.length == pos) {
+        
+        if(slate.size() == nums.length) {
             result.add(new ArrayList<Integer>(slate));
             return;
         }
-
-        //Recursive case 
-        for(int i = 0 ; i < nums.length ; i++) {
-            if(!slate.contains(nums[i])) {
-                slate.add(nums[i]);
-                backtrack(nums, pos+1, slate, result);
-                slate.remove(slate.size() - 1);
+        
+        for(int i = 0; i < nums.length; i++) {
+            if(used[i]) {
+                continue;
             }
+            
+            used[i] = true;
+            slate.add(nums[i]);
+            
+            backtrack(
+                nums,
+                result,
+                slate,
+                used
+            );
+            
+           //backtrack  
+           slate.removeLast();
+           
+           used[i] = false;
+          
         }
+        
     }
+    
+        
 }
