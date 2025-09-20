bc69811f830d07fd8e1e0f9e4f69453880b64d76
vikky49 <vikky49@gmail.com>
2025-09-20 18:59:43 +0000

    Sync LeetCode submission Runtime - 45 ms (22.10%), Memory - 46 MB (30.66%)

---
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/solution.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String removeDuplicates(String s, int k) {
+
+        Stack<Pair<Character, Integer>> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+
+            if (!stack.isEmpty() && stack.peek().getKey() == c) {
+                Pair<Character, Integer> top = stack.pop();
+                stack.push(new Pair<>(c, top.getValue() + 1));
+
+                //if the count reaches k elements 
+                if (stack.peek().getValue() == k) {
+                    stack.pop();
+                }
+
+            } else {
+                stack.push(new Pair<>(c, 1));
+
+            }
+
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (Pair<Character, Integer> pair : stack) {
+            char c = pair.getKey();
+            int value = pair.getValue();
+
+            sb.append(String.valueOf(c).repeat(Math.max(0, value)));
+
+        }
+
+        return sb.toString();
+
+    }
+}
