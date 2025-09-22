7a4848eb4eded07fd09be50911627ebf2899f4ec
vikky49 <vikky49@gmail.com>
2025-09-22 23:51:14 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 41.5 MB (20.57%)

--- 0100-same-tree/solution.java
+++ 0100-same-tree/solution.java
@@ -14,74 +14,31 @@
  * }
  */
 class Solution {
-    
-    class TwoTrees {
-        
-        TreeNode p;
-        TreeNode q;
-        
-        TwoTrees(TreeNode p, TreeNode q) {
-            this.p = p;
-            this.q = q;
-        }
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return dfs(p,q);
         
     }
-    public boolean isSameTree(TreeNode p, TreeNode q) {
+    
+    private boolean dfs(TreeNode p, TreeNode q) {
         
-        //base case
+        //Base cases 
         
-        if (p == null && q == null) {
+        if(p == null && q == null) {
             return true;
         }
         
-        if(( p == null && q != null) || (p != null && q == null)) {
+        if(p == null || q == null) {
             return false;
         }
         
-        
-        TwoTrees twoTrees = new TwoTrees(p,q);
-        
-        Queue<TwoTrees> queue = new LinkedList<>();
-        queue.add(twoTrees);
-        
-        while(!queue.isEmpty()) {
-            
-            int size = queue.size();
-            
-            for(int i=0 ; i < size ; i++) {
-                
-                TwoTrees current = queue.remove();
-                
-                TreeNode tree1 = current.p;
-                
-                TreeNode tree2 = current.q;
-                
-                //case 1: both tree nodes have left child
-                if(tree1.left != null && tree2.left != null) {
-                    queue.add(new TwoTrees(tree1.left, tree2.left));
-                }
-                //case 2: either left or right subtree does not have a left child
-                else if(tree1.left != null || tree2.left!= null) {
-                    return false;
-                }
-                
-                //case 3: Both trees have right subchild
-                if(tree1.right != null && tree2.right != null) {
-                    queue.add(new TwoTrees(tree1.right,tree2.right));
-                }
-                // Case 4: Either one of the tree does not have the right subtree
-                else if (tree1.right != null || tree2.right != null) {
-                    return false;
-                }
-                
-                if(tree1.val != tree2.val){
-                    return false;
-                }
-                                            
-            }
-            
+        if(p.val != q.val) {
+            return false ;
         }
-        return true;
-                        
+        
+        boolean leftSame = dfs(p.left,q.left);
+        boolean rightSame = dfs(p.right,q.right);
+        
+        return leftSame && rightSame;
+        
     }
 }
