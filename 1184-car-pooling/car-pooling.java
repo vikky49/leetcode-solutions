77bc015c601303981d5e2d6448effe9dc79cb6b6
vikky49 <vikky49@gmail.com>
2025-10-20 10:24:00 -0500

    Time: 6 ms (44.38%) | Memory: 44.2 MB (85.89%) - LeetSync

---
+++ 1184-car-pooling/car-pooling.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        Map<Integer, Integer> changes = new HashMap<>();
+
+        for (int[] trip : trips) {
+
+            int passengers = trip[0];
+            int from = trip[1];
+            int to = trip[2];
+
+            changes.put(from, changes.getOrDefault(from, 0) + passengers);
+            changes.put(to, changes.getOrDefault(to, 0) - passengers);
+
+        }
+
+        List<Integer> locations = new ArrayList<>(changes.keySet());
+        Collections.sort(locations);
+
+        int currentPassengers = 0;
+        for (int location : locations) {
+            currentPassengers += changes.get(location);
+            if (currentPassengers > capacity) {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+}
