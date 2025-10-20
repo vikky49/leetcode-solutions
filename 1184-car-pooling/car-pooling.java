96b6dc82c7f9fb226d395f39732a65738bc5c60f
vikky49 <vikky49@gmail.com>
2025-10-20 10:27:54 -0500

    Time: 1 ms (100.00%) | Memory: 44.4 MB (60.98%) - LeetSync

--- 1184-car-pooling/car-pooling.java
+++ 1184-car-pooling/car-pooling.java
@@ -1,30 +1,55 @@
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
-        Map<Integer, Integer> changes = new HashMap<>();
+
+        int[] changes = new int[1001];
 
         for (int[] trip : trips) {
-
             int passengers = trip[0];
             int from = trip[1];
             int to = trip[2];
 
-            changes.put(from, changes.getOrDefault(from, 0) + passengers);
-            changes.put(to, changes.getOrDefault(to, 0) - passengers);
+            changes[from] += passengers;
+            changes[to] -= passengers;
 
         }
 
-        List<Integer> locations = new ArrayList<>(changes.keySet());
-        Collections.sort(locations);
-
         int currentPassengers = 0;
-        for (int location : locations) {
-            currentPassengers += changes.get(location);
+        for (int change : changes) {
+            currentPassengers += change;
+
             if (currentPassengers > capacity) {
                 return false;
             }
         }
-
         return true;
-
     }
 }
+
+//Alternative Solution with hashmap
+/*    Map<Integer, Integer> changes = new HashMap<>();
+
+    for (int[] trip : trips) {
+
+        int passengers = trip[0];
+        int from = trip[1];
+        int to = trip[2];
+
+        changes.put(from, changes.getOrDefault(from, 0) + passengers);
+        changes.put(to, changes.getOrDefault(to, 0) - passengers);
+
+    }
+
+    List<Integer> locations = new ArrayList<>(changes.keySet());
+    Collections.sort(locations);
+
+    int currentPassengers = 0;
+    for (int location : locations) {
+        currentPassengers += changes.get(location);
+        if (currentPassengers > capacity) {
+            return false;
+        }
+    }
+
+    return true;
+
+} */
